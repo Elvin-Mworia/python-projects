cb3e26e73c1f6951350b02acfca9a05531a7ead4
Elvin-Mworia <elvinmworia@gmail.com>
2025-01-09 11:55:58 +0300

    changed the enqueue method to use inbount_stack list to push elements in the queue

--- Queues.py
+++ Queues.py
@@ -2,12 +2,12 @@ class Queue:
    def __init__(self):
       self.inbound_stack=[]
       self.outbound_stack=[]
-      self.size=0
+     
     
   #adding an element in front of the queue
    def enqueue(self,data):
-      self.items.insert(0,data)
-      self.size+=1
+      self.inbound_stack.append(data)
+      
  
    #removing an last element from the queue
    def dequeue(self):
