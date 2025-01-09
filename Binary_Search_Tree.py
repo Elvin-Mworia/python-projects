5339ff6920b40a0de51967fdf39b7a1a05e9ba5d
Elvin-Mworia <elvinmworia@gmail.com>
2025-01-09 16:15:04 +0300

    skeleton

---
+++ Binary_Search_Tree.py
@@ -0,0 +1,9 @@
+class Node:
+    def __init__(self,data=None):
+        self.data=data
+        self.next=None
+class Binary_Search_Tree(Node):
+    def __init__(self):
+        self.root_node=None
+
+   
