0330780e60547039d2b869d3fb0d5873b6cf80e6
Elvin-Mworia <elvinmworia@gmail.com>
2025-01-09 11:49:23 +0300

    implemented two stacks to support  push and pop operations

--- Queues.py
+++ Queues.py
@@ -1,6 +1,7 @@
 class Queue:
    def __init__(self):
-      self.items=[]
+      self.inbound_stack=[]
+      self.outbound_stack=[]
       self.size=0
     
   #adding an element in front of the queue
@@ -8,7 +9,7 @@ class Queue:
       self.items.insert(0,data)
       self.size+=1
  
-#removing an last element from the queue
+   #removing an last element from the queue
    def dequeue(self):
      data=self.items.pop()
      self.size-=1
