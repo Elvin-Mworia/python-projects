e52d51f599886664a979b835ef18359ea2e4276d
Elvin-Mworia <elvinmworia@gmail.com>
2025-01-09 19:34:13 +0300

    insert method

--- Binary_Search_Tree.py
+++ Binary_Search_Tree.py
@@ -13,3 +13,23 @@ class Binary_Search_Tree(Node):
                 current=current.left_child
             
             return current
+    
+    def insert(self,data):
+         node=Node(data)
+         if self.root_node is None:
+              self.root_node=node
+         else:
+              current=self.root_node
+              parent=None
+              while True:
+                   parent=current
+                   if node.data < current.data:
+                        current=current.left_child
+                        if current is None:
+                            parent.left_child=node
+                            return
+                   else:
+                        current=current.right_child
+                        if current is None:
+                            parent.right_child=node
+                            return 
