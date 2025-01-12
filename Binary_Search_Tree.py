3448f4434004f9766c604061b9593f2587ed09ad
Elvin-Mworia <elvinmworia@gmail.com>
2025-01-12 11:30:36 +0300

    method for removing a node

--- Binary_Search_Tree.py
+++ Binary_Search_Tree.py
@@ -41,12 +41,63 @@ class Binary_Search_Tree(Node):
             return (parent,None)
         while True:
             if current.data==data:
-                   return (parent,current)
+                return (parent,current)
             elif current.data>data:
-                   parent=current
-                   current=current.left_child
+                parent=current
+                current=current.left_child
             else:
-                   parent=current
-                   current=current.right_child
-            return(parent,current)
+                parent=current
+                current=current.right_child
+            
+        return parent,current
         
+    def remove(self,data):
+         parent,node =self.get_node_with_parent(data)
+         if parent is None and node is None:
+              return False
+         #children count
+         children_count=0
+
+         if node.left_child and node.right_child:
+              children_count=2
+         elif (node.left_child is None and node.right_child is None):
+              children_count=0
+         else:
+              children_count=1
+        
+         if  children_count==0:
+            if parent:
+                 if parent.right_child is node:
+                      parent.right_child=None
+                 else:
+                      parent.left_child=None
+            else:
+                self.root_node=None
+         elif children_count==1:
+              next_node=None
+              if node.left_child:
+                   next_node=node.left_child
+              else:
+                   next_node=node.right_child
+              if parent:
+                   if parent.left_child is node:
+                        parent.left_child=next_node
+                   else:
+                        parent.right_child=next_node
+              else:
+                   self.root_node=next_node
+                        
+         else:
+              parent_of_leftmost_node=node
+              leftmost_node=node.right_child
+              while leftmost_node.left_child:
+                   parent_of_leftmost_node=leftmost_node
+                   leftmost_node=leftmost_node.left_child
+
+              node.data=leftmost_node.data
+
+         if parent_of_leftmost_node.left_child==leftmost_node:
+            parent_of_leftmost_node.left_child=leftmost_node.right_child
+         else:
+              parent_of_leftmost_node.right_child=leftmost_node.right_child
+              
