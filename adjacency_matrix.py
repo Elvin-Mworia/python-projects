b6ca13b3c048082d577e99ebb175050c30c181d3
Elvin-Mworia <elvinmworia@gmail.com>
2025-01-17 14:07:12 +0300

    filling the adjacency matrix

--- adjacency_matrix.py
+++ adjacency_matrix.py
@@ -6,13 +6,27 @@ graph['E']=['B','C']
 graph['F']=['C']
 
 matrix_elements=sorted(graph.keys())
+print(matrix_elements)
 cols=rows=len(matrix_elements)
+print("{} {}".format(cols,rows))
 
 adjacency_matrix=[[0 for x  in range(rows)] for y in range(cols)]
 edges_list=[]
 
 for key in matrix_elements:
+    print(graph[key])
     for neighbor in graph[key]:
-        edges_list.append({key,neighbor})
+        edge=[]
+        edge.append(key)
+        edge.append(neighbor)
+        print("{} {}".format(key,neighbor))
+        edges_list.append(edge)
 
 print(edges_list)
+for edge in edges_list:
+    index_of_first_vertex=matrix_elements.index(edge[0])
+    index_of_second_vertex=matrix_elements.index(edge[1])
+    print("index of first vertex:{}  index of second vertex:{}".format(index_of_first_vertex,index_of_second_vertex))
+    adjacency_matrix[index_of_first_vertex][index_of_second_vertex]=1
+
+print(adjacency_matrix)
