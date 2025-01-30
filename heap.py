1dd7dcbf67d4d2a501639726997b2b149e93d1b0
Elvin-Mworia <elvinmworia@gmail.com>
2025-01-30 11:10:59 +0300

    swaps elements in the heap if the element inserted is greater than the parent element

--- heap.py
+++ heap.py
@@ -3,3 +3,9 @@ class Heap:
         self.heap=[0]
         self.size=0
         
+    def float(self,k):
+        while (k//2>0):
+            if(self.heap[k] <self.heap[k//2]):
+                self.heap[k],self.heap[k//2]=self.heap[k//2],self.heap[k]
+            k//=2
+
