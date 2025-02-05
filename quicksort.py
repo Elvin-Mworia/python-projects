956d78df1a0457eb55b8b39f83fdcbd2701333a6
Elvin-Mworia <elvinmworia@gmail.com>
2025-02-05 19:39:05 +0300

    quick sort algorithm

---
+++ quicksort.py
@@ -0,0 +1,43 @@
+import random
+def quickSort(unsorted_array,first,last):
+    if last-first <=0:
+        return 
+    else:
+        partition_point=partition(unsorted_array,first,last) 
+        print(unsorted_array)
+        quickSort(unsorted_array,first,partition_point-1)
+        quickSort(unsorted_array,partition_point+1,last)     
+
+def partition(unsorted_array,first_index,last_index):
+    pivot=unsorted_array[first_index]
+    pivot_index=first_index
+    index_of_last_element=last_index
+
+    less_than_pivot_index=index_of_last_element
+    greater_than_pivot_index=first_index+1
+
+    while True:
+
+        while unsorted_array[greater_than_pivot_index]< pivot and greater_than_pivot_index<last_index:
+            greater_than_pivot_index+=1
+        
+        while unsorted_array[less_than_pivot_index]>pivot and less_than_pivot_index>=first_index:
+            less_than_pivot_index-=1
+
+        if greater_than_pivot_index<less_than_pivot_index:
+            temp=unsorted_array[greater_than_pivot_index]
+            unsorted_array[greater_than_pivot_index]=unsorted_array[less_than_pivot_index]
+            unsorted_array[less_than_pivot_index]=temp
+
+        else:
+            break
+    unsorted_array[pivot_index]=unsorted_array[less_than_pivot_index]
+    unsorted_array[less_than_pivot_index]=pivot
+    return less_than_pivot_index
+import random
+
+# 1000 random numbers not exceeding 100000
+random_numbers = [random.randint(0, 100000) for _ in range(100)]
+
+
+quickSort(random_numbers,0,len(random_numbers)-1)
