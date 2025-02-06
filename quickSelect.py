6d6615b54b0010f8163a4955046137f835d17e52
Elvin-Mworia <elvinmworia@gmail.com>
2025-02-06 23:59:57 +0300

    partion function to select the split element

--- quickSelect.py
+++ quickSelect.py
@@ -13,5 +13,33 @@ def quickSelect(unsorted_array,left,right,k):
 
 
 
-def partition(unsorted_array,first,last):
-    return
+def partition(unsorted_array,first_index,last_index):
+    if first_index==last_index:
+        return first_index
+    
+    pivot=unsorted_array[first_index]
+    pivot_index=first_index
+    index_of_last_element=last_index
+
+    less_than_pivot_index=index_of_last_element
+    greater_than_pivot_index=first_index+1
+
+    while True:
+
+        while unsorted_array[greater_than_pivot_index]< pivot and greater_than_pivot_index<last_index:
+            greater_than_pivot_index+=1
+        
+        while unsorted_array[less_than_pivot_index]>pivot and less_than_pivot_index>=first_index:
+            less_than_pivot_index-=1
+
+        if greater_than_pivot_index<less_than_pivot_index:
+            temp=unsorted_array[greater_than_pivot_index]
+            unsorted_array[greater_than_pivot_index]=unsorted_array[less_than_pivot_index]
+            unsorted_array[less_than_pivot_index]=temp
+        else:
+            break
+
+        unsorted_array[pivot_index]=unsorted_array[less_than_pivot_index]
+        unsorted_array[less_than_pivot_index]=pivot
+
+        return less_than_pivot_index
